4e785941fe5a1cb0900e36783003ae70ac75f2f1
JackLiR8 <jiakeli1024@gmail.com>
2019-12-25 16:45:23 +0800

    :truck:

--- src/api/index.js
+++ src/api/index.js
@@ -41,74 +41,15 @@ AXI.interceptors.response.use(
  * @param {Object} params [请求时携带的参数]
  */
 export function get(url, params) {
-    return new Promise((resolve, reject) => {
-        AXI.get(url, {
-            params: params
+    return AXI.get(url, {
+            params
         })
-        .then(res => {
-            resolve(res.data);
-        })
-        .catch(err => {
-            reject(err.data)
-        })
-    })
+        .then(res => res.data)
 }
 
 export const del = (url, params) => {
     return AXI.delete(url, {
-            data:params
+            params
         })
         .then(res => res.data);
-}
-
-
-/** 
- * post方法，对应post请求 
- * @param {String} url [请求的url地址] 
- * @param {Object} params [请求时携带的参数] 
- */
-export function post(url, params) {
-    return new Promise((resolve, reject) => {
-        AXI.post(url, params)
-            .then(res => {
-                resolve(res.data);
-            })
-            .catch(err => {
-                reject(err.data)
-            })
-    });
-}
-
-/** 
- * put方法，对应put请求 
- * @param {String} url [请求的url地址] 
- * @param {Object} params [请求时携带的参数] 
- */
-export function put(url, params) {
-    return new Promise((resolve, reject) => {
-        AXI.put(url, JSON.stringify(params))
-            .then(res => {
-                resolve(res.data);
-            })
-            .catch(err => {
-                reject(err.data)
-            })
-    });
-}
-
-/** 
- * patch方法，对应patch请求 
- * @param {String} url [请求的url地址] 
- * @param {Object} params [请求时携带的参数] 
- */
-export function patch(url, params) {
-    return new Promise((resolve, reject) => {
-        AXI.patch(url, JSON.stringify(params))
-            .then(res => {
-                resolve(res.data);
-            })
-            .catch(err => {
-                reject(err.data)
-            })
-    });
 }
